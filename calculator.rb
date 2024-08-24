84603476d960e10ebba21222adba7d54fddbb407
gokutheengineer <engineerzubizaretta@gmail.com>
2024-08-24 23:17:02 +0300

    feat: calc

---
+++ calculator.rb
@@ -0,0 +1,52 @@
+class Calculator
+  def initialize
+    @result = 0
+  end
+
+  def add(num)
+    @result += num
+    self
+  end
+
+  def subtract(num)
+    @result -= num
+    self
+  end
+
+  def multiply(num)
+    @result *= num
+    self
+  end
+
+
+  def divide(num)
+    @result /= num
+    self
+  end
+
+  def factorial_inject(num)
+    @result = (1..num).inject(:*)
+    self
+  end
+
+  def factorial_reduce(num)
+    @result = (1..num).reduce(1, :*)
+    self
+  end
+
+  def display_result
+    puts "Current Result: #{@result}"
+    self
+  end
+
+  def reset
+    @result = 0
+    self
+  end
+
+end
+
+calc = Calculator.new
+calc.add(5).add(10).display_result
+
+calc.reset.factorial_reduce(5).display_result
